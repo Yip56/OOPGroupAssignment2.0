7c467b3b75278e9efe78af863175c5f6880cc04a
Pakdad Mousavi <pakdad.mousavi@gmail.com>
2025-09-09 16:27:27 +0800

    Complete user authentication.

--- src/main/java/my/edu/apu/controllers/AuthController.java
+++ src/main/java/my/edu/apu/controllers/AuthController.java
@@ -4,6 +4,10 @@
  */
 package my.edu.apu.controllers;
 
+import java.util.Optional;
+import javax.swing.JOptionPane;
+import javax.swing.SwingUtilities;
+import my.edu.apu.models.User;
 import my.edu.apu.repositories.UserRepository;
 import my.edu.apu.views.LoginFrame;
 import my.edu.apu.views.MainFrame;
@@ -15,20 +19,44 @@ import my.edu.apu.views.MainFrame;
 public class AuthController {
 
     private final LoginFrame loginFrame;
+    private final UserRepository userRepo;
 
     public AuthController(LoginFrame loginFrame, UserRepository userRepo) {
         this.loginFrame = loginFrame;
+        this.userRepo = userRepo;
 
+        // Authenticate the user when the login button is pressed
         loginFrame.getLoginButton().addActionListener(e -> authenticate());
     }
 
     private void authenticate() {
-        String username = loginFrame.getUniEmail();
+        String uniEmail = loginFrame.getUniEmail();
         String password = loginFrame.getPassword();
 
-        if (username.equals("Jack") && password.equals("secret")) {
-            loginFrame.dispose();
-            new MainFrame().setVisible(true);
+        Optional<User> userOpt = userRepo.findByUniEmail(uniEmail);
+        userOpt.ifPresentOrElse(
+                (user) -> handleAuthentication(user, password),
+                () -> displayErrorDialog("Your email could not be found, please try again.")
+        );
+    }
+
+    private void handleAuthentication(User user, String password) {
+        if (user.getPassword().equals(password)) {
+            displayMainFrame(user);
+        } else {
+            displayErrorDialog("Your password is incorrect, please try again.");
         }
+    }
+
+    private void displayMainFrame(User user) {
+        SwingUtilities.invokeLater(() -> {
+            loginFrame.dispose();
+            MainFrame mainFrame = new MainFrame();
+            mainFrame.setVisible(true);
+        });
+    }
+
+    private void displayErrorDialog(String msg) {
+        JOptionPane.showMessageDialog(loginFrame, msg, "Login Error", JOptionPane.ERROR_MESSAGE);
     }
 }
